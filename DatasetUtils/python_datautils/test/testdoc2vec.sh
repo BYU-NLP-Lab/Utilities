28d3424d4953e16bb2f0630ddf8ff764857e98ed
Paul Felt <paul.lewis.felt@gmail.com>
2015-05-14 11:18:49 -0600

    small fix to test script

--- DatasetUtils/python_datautils/test/testdoc2vec.sh
+++ DatasetUtils/python_datautils/test/testdoc2vec.sh
@@ -6,6 +6,6 @@ pushd ..; ./doc2vec.py --dataset-basedir=/aml/data/newsgroups --dataset-split /a
 pushd ..; ./doc2vec.py --dataset-basedir=/aml/data/newsgroups --dataset-split /aml/data/newsgroups/indices/tiny_set --method=PARAVEC --outdir=/tmp/d2v-tiny; popd
 
 ## generate models on newsgroups-full
-#../doc2vec.py --dataset-basedir=/aml/data/newsgroups --dataset-split /aml/data/newsgroups/indices/full_set/all --method=LDA --out=/tmp/lda-full
-#../doc2vec.py --dataset-basedir=/aml/data/newsgroups --dataset-split /aml/data/newsgroups/indices/full_set/all --method=WORD2VEC --out=/tmp/w2v-full
-#../doc2vec.py --dataset-basedir=/aml/data/newsgroups --dataset-split /aml/data/newsgroups/indices/full_set/all --method=PARAVEC --out=/tmp/d2v-full
+#pushd ..; ./doc2vec.py --dataset-basedir=/aml/data/newsgroups --dataset-split /aml/data/newsgroups/indices/full_set/all --method=LDA --out=/tmp/lda-full; popd
+#pushd ..; ./doc2vec.py --dataset-basedir=/aml/data/newsgroups --dataset-split /aml/data/newsgroups/indices/full_set/all --method=WORD2VEC --out=/tmp/w2v-full; popd
+#pushd ..; ./doc2vec.py --dataset-basedir=/aml/data/newsgroups --dataset-split /aml/data/newsgroups/indices/full_set/all --method=PARAVEC --out=/tmp/d2v-full; popd
